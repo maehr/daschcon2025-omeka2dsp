0610bbfba17114c83acf931aa45186ae3401e26a
Moritz Mähr <maehr@users.noreply.github.com>
2025-10-14 17:07:39 +0200

    Built site for gh-pages

---
+++ site_libs/revealjs/plugin/math/plugin.js
@@ -0,0 +1,4 @@
+import{KaTeX as a}from"./katex";import{MathJax2 as t}from"./mathjax2";import{MathJax3 as e}from"./mathjax3";const o=t;/*!
+ * This plugin is a wrapper for the MathJax2,
+ * MathJax3 and KaTeX typesetter plugins.
+ */export default Plugin=Object.assign(o(),{KaTeX:a,MathJax2:t,MathJax3:e});
